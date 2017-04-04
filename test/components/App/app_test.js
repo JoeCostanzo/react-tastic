dc4415f56d49915b0a646b6122a10fbd0a5e570c
joeco <joe@joeco.info>
2017-04-04 11:26:24 -0400

    updates test

--- test/components/App/app_test.js
+++ test/components/App/app_test.js
@@ -2,7 +2,6 @@ import expect from 'expect.js';
 import React from 'react';
 import { renderShallow } from '../../test_helper';
 import App from '../../../src/components/App';
-import Welcome from '../../../src/components/Welcome';
 
 
 describe('components/App', () => {
@@ -10,13 +9,16 @@ describe('components/App', () => {
 
   beforeEach(() => subject = renderShallow(<App/>));
 
+  it('works', () => expect(1).to.eql(1));
+
   it('renders as a div', () =>
     expect(subject.type).to.be('div'));
 
-  it('renders as a welcome component as a child', () =>
-    expect(subject.props.children).to.eql(<Welcome />));
-
-  it('works', () => expect(1).to.eql(1));
+  it('has h1 & p`s as children', () => {
+    expect(subject.props.children[0].type).to.eql('h1');
+    expect(subject.props.children[1].type).to.eql('p');
+    expect(subject.props.children[2].type).to.eql('p');
+  });
 });
 /**
  * Created by joec on 3/31/2017.
